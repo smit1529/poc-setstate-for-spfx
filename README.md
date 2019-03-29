45c2219187a5615df76e3b0b47515e4c36d721ef
smit1529 <smit.pshah92@gmail.com>
2019-03-29 19:25:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# poc-setstate-for-spfx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/poc-setstate-for-spfx)
---
+++ index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React',
+      data: []
+    };
+  }
+
+  componentDidMount(){
+console.log('componentDidMount Fired');   
+var temp = this.state.data;
+temp.push(6)
+this.setState(
+  {data: temp}
+)
+temp.push(6)
+this.setState(
+  {data: temp}
+)
+  }
+
+  componentDidUpdate(){
+    console.log('componentDidUpdate Fired');   
+
+  }
+
+clickButton(){
+console.log('clciked');
+var temp = this.state.data;
+temp.push(6)
+// this.setState(
+//   {data: temp}
+// )
+}
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          <input type="button" value="Submit" onClick={()=>this.clickButton()} />
+        </p>
+
+        {this.state.data.map((item)=>{
+          return(
+<p>{item}</p>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
