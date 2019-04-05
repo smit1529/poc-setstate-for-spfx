2b87464ed990705bc20a1d19300f15fc4dcf83b6
smit1529 <smit.pshah92@gmail.com>
2019-04-05 16:13:08 +0530

    1 file modified

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ class App extends Component {
     var temp = this.state.data;
     // temp.push(temp.length);
     for (i = 1; i <= 5; i++) {
-      temp.push(temp.length);
+      temp.push(temp.length + 1);
     }
     this.setState(
       { data: temp }
@@ -58,26 +58,27 @@ class App extends Component {
   render() {
     console.log('render');
 
+
     return (
       <div>
         <Hello name={this.state.name} />
 
-        {this.state.data.map((item) => {
-          MapFunctionCounter = MapFunctionCounter + 1;
-
-          return (
-            <table>
-              <tbody>
-                <tr>
-                  <td style={{ border: "1px solid green" }}>
-                    {"_" + item + "_"}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          );
-        })}
-
+        <div style={{ overflow: "auto", height: "250px" }}>
+          <table>
+            <tbody>
+              {this.state.data.map((item) => {
+                return (
+                  <tr>
+                    <td style={{ border: "1px solid green" }}>
+                      {"_" + item + "_"}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        
         <p>
           <input type="button" value="Submit" onClick={() => this.clickButton()} />
         </p>
